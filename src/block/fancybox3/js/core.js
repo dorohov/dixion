b2e35c9d19ba2f16f25a3b82de11795c891f321d
caezar47 <iss47@ya.ru>
2017-06-09 07:57:49 +0300

    add new edit's

--- src/block/fancybox3/js/core.js
+++ src/block/fancybox3/js/core.js
@@ -98,17 +98,19 @@
                 '<div class="fancybox-bg"></div>' +
                 '<div class="fancybox-controls">' +
                     '<div class="fancybox-infobar">' +
-                        '<button data-fancybox-previous class="fancybox-button fancybox-button--left" title="Previous"></button>' +
+                       
                         '<div class="fancybox-infobar__body">' +
                             '<span class="js-fancybox-index"></span>&nbsp;/&nbsp;<span class="js-fancybox-count"></span>' +
                         '</div>' +
-                        '<button data-fancybox-next class="fancybox-button fancybox-button--right" title="Next"></button>' +
+                        
                     '</div>' +
                     '<div class="fancybox-buttons">' +
                         '<button data-fancybox-close class="fancybox-button fancybox-button--close" title="Close (Esc)"></button>' +
                     '</div>' +
                 '</div>' +
                 '<div class="fancybox-slider-wrap">' +
+                    '<button data-fancybox-next class="fancybox-button fancybox-button--right" title="Next"></button>' +
+                    '<button data-fancybox-previous class="fancybox-button fancybox-button--left" title="Previous"></button>' +
                     '<div class="fancybox-slider"></div>' +
                 '</div>' +
                 '<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
