d7975019a0e1a334d6598c0b1e9f3ad067342fc5
caezar47 <iss47@ya.ru>
2017-08-01 12:15:09 +0300

    add animation in tour

---
+++ src/block/site/default/tour/.document-ready.js
@@ -0,0 +1,6 @@
+$(".tour-item__item").on('mouseover', function(){
+	$(this).toggleClass("is--hover");
+});
+$(".tour-item__item").on('mouseout', function(){
+	$(this).toggleClass("is--hover"); 
+});
